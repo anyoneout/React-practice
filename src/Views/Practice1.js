d8ff21b79f8971218fe37646bfb5a4bb9531d66d
anyoneout <chrisdafur@gmail.com>
2025-01-23 14:31:00 -0600

    added Practice1.js page and added BrowserRouter route for it

---
+++ src/Views/Practice1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Practice1() {
+  return (
+    <>
+      <h1>Practice1 heading</h1>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import Level2 from "./Views/Level2";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Practice1 from "./Views/Practice1";
 
 
 
 const root = createRoot(window.bodyTag);
 root.render(
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route>
-          <Level2 />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </>
+
+  <BrowserRouter>
+    <Routes>
+      <Route path="/Level2" element={<Level2 />} />
+      <Route path="/Practice1" element={<Practice1 />} />
+    </Routes>
+  </BrowserRouter>
+
 );
 
