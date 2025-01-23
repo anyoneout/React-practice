ba35aeca26e9be22600f0fab78bfcc3615cd2953
anyoneout <chrisdafur@gmail.com>
2025-01-22 21:26:53 -0600

    added browser router, routes and level 2 route to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 import Level2 from "./Views/Level2";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 
 
 const root = createRoot(window.bodyTag);
 root.render(
   <>
-    <Level2 />
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Level2 />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </>
 );
 
